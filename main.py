9b84655b7a861eca4b0bbba0b78740a1b59dee34
Wilerb Andre <andrecarlya@gmailcom>
2026-06-14 15:56:53 -0400

    fixed  error in line 9

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ sandwich_finished = [ ]
 while len(sandwich_orders) > 0:
     sandwich_finished.append(sandwich_orders[0])
     sandwich_orders.remove(sandwich_orders[0])
-    print(f"your {sandwich_orders[0]} is ready")
+    print(f"your {sandwich_finished[-1]} is ready")
     
 
