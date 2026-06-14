5751ef9b7b6834f41450d580402f6a4a0dcde8f1
Wilerb Andre <andrecarlya@gmailcom>
2026-06-14 16:02:50 -0400

    Printed lists for verification

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-sandwich_orders = ["tuna", "chicken", "Ham"]
+sandwich_orders = ["tuna", "chicken", "Ham" ]
 sandwich_finished = [ ]
-
-
 
 while len(sandwich_orders) > 0:
     sandwich_finished.append(sandwich_orders[0])
     sandwich_orders.remove(sandwich_orders[0])
-    print(f"your {sandwich_finished[-1]} is ready")
-    
+    print(f"The {sandwich_finished[-1]} SandwichFinal version ready")
+
+print(f"\nThe following sandwiches were made:")
+print(*sandwich_finished)
 
