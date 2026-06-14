b1b4533655b1f6915fc682771ac09bd38cf99330
Wilerb Andre <andrecarlya@gmailcom>
2026-06-14 16:55:44 -0400

    fixed typo

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ sandwich_finished = [ ]
 while len(sandwich_orders) > 0:
     sandwich_finished.append(sandwich_orders[0])
     sandwich_orders.remove(sandwich_orders[0])
-    print(f"The {sandwich_finished[-1]} SandwichFinal version ready")
+    print(f"The {sandwich_finished[-1]} Sandwich ready")
 
 print(f"\nThe following sandwiches were made:")
 print(*sandwich_finished)
